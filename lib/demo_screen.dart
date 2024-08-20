a7ffa6b929d04951d56ed5a582f6d69d0a484938
romeoagyemang <romeoasiamah79@gmail.com>
2024-08-20 12:22:21 -0500

    made changes to the UI

---
+++ lib/demo_screen.dart
@@ -0,0 +1,428 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class DemoScreen extends ConsumerWidget {
+  const DemoScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Teal background section including AppBar
+            Container(
+              color: Colors.teal,
+              padding: const EdgeInsets.only(
+                  top: 50.0, left: 16.0, right: 16.0, bottom: 16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // Custom AppBar with Back Arrow
+                  Row(
+                    children: [
+                      IconButton(
+                        icon: const Icon(
+                          Icons.arrow_back,
+                          color: Colors.white,
+                          size: 30,
+                        ),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 10),
+                  const Text(
+                    '7/7 Colors',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontFamily: "Roboto",
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 2),
+                  const Text(
+                    'Eat Rainbow',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24,
+                      fontFamily: "Roboto",
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: LinearProgressIndicator(
+                          value: 0.7, // Set the progress value
+                          backgroundColor: Colors.white.withOpacity(0.3),
+                          color: Colors.white,
+                          minHeight: 15,
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+                      const SizedBox(width: 8),
+                      SvgPicture.asset(
+                        'assets/food.svg', // Replace with your image asset
+                        height: 100,
+                        width: 100,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 16),
+            // Right-aligned Date and Calendar icon
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  const Icon(
+                    Icons.calendar_today,
+                    color: Colors.black,
+                  ),
+                  const SizedBox(width: 8),
+                  const Text(
+                    '22 July, 2024',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                      fontFamily: "Roboto",
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 16),
+            // Cards in a row with background color
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Card(
+                      color: Colors.blueGrey[50], // Card background color
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: const Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Expanded(
+                              child: Text(
+                                'Learn more about colors',
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  fontFamily: "Roboto",
+                                  fontWeight: FontWeight.bold,
+                                ),
+                                maxLines:
+                                    2, // Allows text to wrap to the next line if necessary
+                                overflow: TextOverflow.visible,
+                              ),
+                            ),
+                            const SizedBox(width: 8),
+                            CircleAvatar(
+                              backgroundColor:
+                                  Color.fromARGB(255, 221, 220, 220),
+                              radius: 15,
+                              child: const Icon(
+                                Icons.arrow_forward,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  Expanded(
+                    child: Card(
+                      color: Colors.green[50], // Card background color
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Expanded(
+                              child: Text(
+                                'How to register meals',
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  fontFamily: "Roboto",
+                                  fontWeight: FontWeight.bold,
+                                ),
+                                maxLines:
+                                    2, // Allows text to wrap to the next line if necessary
+                                overflow: TextOverflow.visible,
+                              ),
+                            ),
+                            const SizedBox(width: 8),
+                            CircleAvatar(
+                              backgroundColor: Colors.teal,
+                              radius: 15,
+                              child: const Icon(
+                                Icons.arrow_forward,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 30),
+            // Total fibre section with gradient progress bar
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Row(
+                    children: [
+                      Text(
+                        'Total fibre',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontFamily: "Roboto",
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(width: 18),
+                      Text(
+                        '30/60',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontFamily: "Roboto",
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 8),
+                  Container(
+                    height: 15,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [Colors.purple, Colors.pink], // Gradient colors
+                        begin: Alignment.centerLeft,
+                        end: Alignment.centerRight,
+                      ),
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 40),
+            // Row with Dashboard and Show All
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Dashboard',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontFamily: "Roboto",
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    'Show all',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontFamily: "Roboto",
+                      color: Color.fromARGB(255, 243, 33, 240),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 16),
+            // Row with CircleAvatars of different colors, each with '1' as text
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  _buildColorCircle(Colors.red),
+                  _buildColorCircle(Colors.orange),
+                  _buildColorCircle(Colors.yellow),
+                  _buildColorCircle(Colors.green),
+                  _buildColorCircle(Colors.blue),
+                  _buildColorCircle(Colors.brown),
+                  _buildColorCircle(
+                      Color(0xFF7F00FF)), // Rainbow color placeholder
+                ],
+              ),
+            ),
+            const SizedBox(height: 16),
+            // Card with Border, Dropdown, and Details
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  side: BorderSide(color: Colors.grey), // Visible border
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text(
+                            'Red',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontFamily: "Roboto",
+                            ),
+                          ),
+                          IconButton(
+                            icon: Icon(Icons.arrow_drop_down),
+                            onPressed: () {
+                              // Logic for showing the dropdown content
+                              showModalBottomSheet(
+                                context: context,
+                                builder: (context) => _buildDropdownContent(),
+                              );
+                            },
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDropdownContent() {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Card(
+        shape: RoundedRectangleBorder(
+          side: BorderSide(color: Colors.grey),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Beans and Lentiles',
+                style: TextStyle(fontSize: 18, fontFamily: "Roboto"),
+              ),
+              const SizedBox(height: 8),
+              const Text(
+                '146 kcal',
+                style: TextStyle(fontSize: 16, fontFamily: "Roboto"),
+              ),
+              const SizedBox(height: 8),
+              const Text(
+                'Brand',
+                style: TextStyle(fontSize: 16, fontFamily: "Roboto"),
+              ),
+              const SizedBox(height: 8),
+              const Text(
+                'food Functionality',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontFamily: "Roboto",
+                  color: Colors.purple, // Violet color
+                ),
+              ),
+              const SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  _buildSmallCard('A', '85/100', Colors.blue),
+                  _buildSmallCard('GL', '5', Colors.green),
+                ],
+              ),
+              const SizedBox(height: 16),
+              Align(
+                alignment: Alignment.centerRight,
+                child: Icon(
+                  Icons.favorite,
+                  color: Colors.purple, // Violet color for love icon
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSmallCard(String letter, String value, Color bgColor) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        side: BorderSide(color: Colors.grey),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: [
+            CircleAvatar(
+              backgroundColor: bgColor,
+              child: Text(
+                letter,
+                style: const TextStyle(color: Colors.white),
+              ),
+            ),
+            const SizedBox(width: 8),
+            Text(
+              value,
+              style: const TextStyle(fontSize: 16, fontFamily: "Roboto"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// Helper function to build a CircleAvatar with a specific color and '1' as text
+Widget _buildColorCircle(Color color) {
+  return CircleAvatar(
+    radius: 20,
+    backgroundColor: color,
+    child: const Text(
+      '1',
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 16,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+  );
+}
